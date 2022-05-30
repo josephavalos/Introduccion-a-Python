bb51c8f4cef166850d7eb85f84549527cfc2694b
josephavalos <josephavalos792@gmail.com>
2022-05-30 17:44:45 -0600

    metodos interesantes

---
+++ metodos_cadenas_de_caracteres.py
@@ -0,0 +1,24 @@
+# Metodo Capitalize
+"""
+Retorna una copia de la cadena con el primer caracter en mayusculas
+y el resto en minisculas.
+
+Por ejemplo:
+"""
+apellido = 'avalos'
+print(apellido.capitalize())
+
+# Otros Metodos Importantes
+"""
+find: Permite encontrar cadenas o caracteres dentro de otras.
+index: Permite encontrar cadenas o caracteres dentro de otras.
+isalnum: Revisa si la cadena de caracteres solo tiene alfanumericos.
+isalpha: Revisa si solo contiene caracteres alfabeticos.
+isdecimal: Revisa si solo contiene caracteres decimales.
+isdigit: Revisa si solo contiene digitos
+islower: Revisa si solo contiene caracteres en minuscula.
+isupper: Revisa si solo contiene caracteres en mayuscula.
+lower: Retorna una copia en minuscula de la cadena de caracteres.
+upper: Retorna una copia en mayuscula de la cadena de caracteres.
+*Los metodos que tienen la palabra 'is' probablemente retornen verdadero o falso.
+"""
