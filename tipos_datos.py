7b8e0f68a1b8d2756c93ca63b67ff523297c4580
josephavalos <josephavalos792@gmail.com>
2022-05-30 10:44:00 -0600

    funcion type

---
+++ tipos_datos.py
@@ -0,0 +1,13 @@
+variable = 'Jose'
+"""
+Con la funcion type() podemos ver el tipo de datos que guarda la
+variable, en este caso sera class 'str' haciendo referencia a String.
+"""
+print(type(variable))
+
+variable = 2
+"""
+Ahora tiene guardado un 2 por lo tanto tiene pertenecera al tipo
+de dato integer que lo muestra como class 'int'
+"""
+print(type(variable))
