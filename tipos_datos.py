50b4f8ae14fa83fdda52fe0cb466317444ab60c7
josephavalos <josephavalos792@gmail.com>
2022-05-30 11:03:36 -0600

    type(), len()

--- tipos_datos.py
+++ tipos_datos.py
@@ -11,3 +11,16 @@ Ahora tiene guardado un 2 por lo tanto tiene pertenecera al tipo
 de dato integer que lo muestra como class 'int'
 """
 print(type(variable))
+
+
+# Tamaño (Length)
+
+variable = 'Jose'
+"""
+La variable en este caso tiene 4 caracteres formados por la 
+palabra "Jose".
+"""
+print(len(variable))
+
+"Pero si imprimimos un len vacio me va dar cero."
+print(len(""))
