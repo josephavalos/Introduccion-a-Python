cabd101d6c023a3dd2854a3837df157b78a6f070
josephavalos <josephavalos792@gmail.com>
2022-06-07 18:23:13 -0600

    las tuplas

---
+++ tupla.py
@@ -0,0 +1,10 @@
+tupla = (1, 2, 3, 4, 5)
+'Accediendo a sus elementos'
+print(tupla[0])
+print(tupla[1])
+print(tupla[2])
+print(tupla[3])
+print(tupla[4])
+
+'Buscar un elemento en la tupla'
+print(1 in tupla)
